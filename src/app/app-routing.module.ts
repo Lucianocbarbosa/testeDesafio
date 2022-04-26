6852faab95ec12d22d9e0c9904a1514e3c500575
Lucianocbarbosa <lucianobebarbosa@gmail.com>
2022-04-26 00:29:09 -0300

    Criação do componente createPost

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
-import { ReadPostComponent } from './components/post/read-post/read-post.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CreatePostComponent } from './components/post/create-post/create-post.component';
+import { ReadPostComponent } from './components/post/read-post/read-post.component';
+
 const routes: Routes = [
   { path: 'read', component: ReadPostComponent },
+  { path: 'create', component: CreatePostComponent },
   { path: '**', redirectTo: 'read', pathMatch: 'full' },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatePostComponent } from './components/post/create-post/create-post.component';
+import { ReadPostComponent } from './components/post/read-post/read-post.component';
 import { SearchAreaComponent } from './components/search-area/search-area.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { ReadPostComponent } from './components/post/read-post/read-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchAreaComponent,
     SidebarComponent,
-    ReadPostComponent
+    ReadPostComponent,
+    CreatePostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
